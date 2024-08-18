e1c4f4d0b357a881d31e245b402096549e5dd127
Manoranjan kumar <153417773+AimMano@users.noreply.github.com>
2024-08-18 21:20:40 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,39 @@
 
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int number1;
+    cout<<"enter the first number=";
+    cin>>number1;
+    int number2;
+    cout<<"enter the second number=";
+    cin>>number2;
+    char ch;
+    cout<<"Enter the operation to perform:";
+    cin>>ch;
+    //resultant of opertion
+int sum,diff,multi,div;
+    //using switch
+    switch(ch){
+        case '+':
+         sum=number1+number2;
+        cout<<number1<<"+"<<number2<<"="<<sum;
+        break;
+        case '-':
+         diff=number1-number2;
+        cout<<number1<<"-"<<number2<<"="<<diff;
+        break;
+        case '*':
+         multi=number1*number2;
+        cout<<number1<<"*"<<number2<<"="<<multi;
+        break;
+        case '/':
+         div=number1/number2;
+        cout<<number1<<"/"<<number2<<"="<<div;
+        break;
+        defualt:
+        cout<<"Invalid character";
+    }
+    return 0;
+}
